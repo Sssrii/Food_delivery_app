54ab936331aa68e790d98a9df223e6aa73b7ff49
Sssrii <kkajals0944@gmail.com>
2026-07-08 23:24:40 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FoodDel/', // 👈 Replace with your actual GitHub repo name
-  plugins: [react()],
+base: '/Food_delivery_app/',  plugins: [react()],
 })
